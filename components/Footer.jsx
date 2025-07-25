c9f0151ce38c8620449fe63655f988b4cc66c7cc
Palash A Patil <palashp@bitwiseglobal.net>
2025-07-25 10:52:09 +0530

    adding my logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,7 +13,7 @@ function Footer({isDarkMode}) {
         </div>
       </div>
       <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-        <p>© 2025 William Mark. All rights reserved.</p>
+        <p>© 2025 Palash Patil. All rights reserved.</p>
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
             <li>
                 <a target='_blank' href="https://github.com/palashapatil">GitHub</a>
